9cc167139747687d41938176453b5e2b4635c552
sayma_kazi06 <saymakazi6108@gmail.com>
2025-12-16 23:39:23 +0530

    Initial commit

---
+++ Assignment/menu_driven.py
@@ -0,0 +1,15 @@
+num = int(input("Enter your number:"))
+print("Check your entered number is:\n1.Even\n2.Odd");
+choice = int(input("Enter your choice(in number):"))
+
+if choice == 1:
+    if num%2 == 0 :
+        print("Number is even.")
+elif choice == 2:
+    if num%2 != 2:
+        print("Number is Odd")
+elif choice != 1 and choice != 2:
+    print("Invalid input")
+
+
+    
